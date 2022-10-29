7dca4608bf60ddb58dc60c07c02b22cd1ff54ad1
Ganesh <Ganesh@Ganesh>
2022-10-29 20:34:35 +0530

    first repository

---
+++ src/github1/demo1.java
@@ -0,0 +1,17 @@
+package github1;
+
+public class demo1 {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello");
+	}
+	
+	public static void main() {
+		
+		System.out.println("hi");
+	}
+
+}
